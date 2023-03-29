d455d922137d7564f9a556c9b3327b9eac340d7f
teganasprey <82981420+teganasprey@users.noreply.github.com>
2023-03-28 23:07:17 -0400

    Finished

--- src/SListArray.java
+++ src/SListArray.java
@@ -19,7 +19,13 @@ public class SListArray extends SList{
     }
 
     public int size() {
-        return this._elements.length;
+        int size = 0;
+        for (int i=0; i<this._elements.length; i++) {
+            if (this._elements[i] != null) {
+                size++;
+            }
+        }
+        return size;
     }
 
     private String[] copy(int new_size, String[] elements) {
@@ -31,10 +37,16 @@ public class SListArray extends SList{
     }
 
     public String get(int position) {
+        if (position < 0 || position > this._elements.length-1) {
+            return INDEX_OUT_OF_RANGE;
+        }
         return this._elements[position];
     }
 
     public String set(int position, String element) {
+        if (position < 0 || position > this._elements.length-1) {
+            return INDEX_OUT_OF_RANGE;
+        }
         String old_element = this._elements[position];
         this._elements[position] = element;
         return old_element;
@@ -42,38 +54,88 @@ public class SListArray extends SList{
 
     public String add(int position, String element)
     {
-        if(position<0||position>this.size())
-            return "Index out of bounds";
-        if (this.get(this.size()-1)!=null)
-        {
-            SListArray myList = new SListArray();
-            for (int i=0;i<index-1; i++)
-                myList.set(i,this.get(i));
-
-        }
+        if(position < 0 || position > this._elements.length-1)
+            return INDEX_OUT_OF_RANGE;
+        if (this._elements[this._elements.length-1] != null)
+            this._elements = copy((this._elements.length)*2, this._elements);
+        for (int i=position+1; i<this._elements.length-1; i++)
+            this._elements[i+1] = this._elements[i];
+        this._elements[position] = element;
+        return INDEX_OK;
     }
-
     public String remove(int position) {
-
+        if(position < 0 || position > this._elements.length-1)
+            return INDEX_OUT_OF_RANGE;
+        for (int i = this._elements.length-1; i>position; i--)
+            this._elements[i-1] = this._elements[i];
+        return INDEX_OK;
     }
 
     public void append(SList anotherSList)
     {
-        int newSize=(anotherSList.size()+this.size())*2;
-        SListArray myList= new SListArray;
-        for (int i=0;i<this.size();i++)
-            myList.set(i,this.get(i));
-        for (int i=this.size();i<(anotherSList.size()+this.size());i++)
-            myList.set(i,anotherSList.get(i));
-
+        int new_size = (anotherSList.size() + this.size()) * 2;
+        String[] new_elements = new String[new_size];
+        for (int i=0; i<this.size(); i++) {
+            new_elements[i] = this._elements[i];
+        }
+        for (int i=this.size(); i<(anotherSList.size()+this.size()); i++) {
+            new_elements[i] = anotherSList.get(i-this.size());
+        }
+        this._elements = new_elements;
     }
-
     public SList commonStrings()
     {
+        HashMap<String, Integer> dict = new HashMap<>();
+        for (int i=0; i<this._elements.length; i++) {
+            if (dict.containsKey(this._elements[i])) {
+                dict.put(this._elements[i], dict.get(this._elements[i] + 1));
+            }
+            else {
+                dict.put(this._elements[i], 1);
+            }
+        }
 
+        int highest_frequency = 0;
+        for (int value: dict.values()) {
+            if (value > highest_frequency)
+                highest_frequency = value;
+        }
+
+        SListArray commonWords = new SListArray();
+        for (String s: dict.keySet()) {
+            if (dict.get(s) == highest_frequency) {
+                commonWords.add(commonWords.size()-1, s);
+            }
+        }
+        return commonWords;
     }
 
     public SList[] commonStrings(int n) {
+        HashMap<String, Integer> dict = new HashMap<>();
+        for (int i=0; i<this._elements.length; i++) {
+            if (dict.containsKey(this._elements[i])) {
+                dict.put(this._elements[i], dict.get(this._elements[i] + 1));
+            }
+            else {
+                dict.put(this._elements[i], 1);
+            }
+        }
 
+        ArrayList<Integer> frequencies = new ArrayList<>(dict.values());
+        Collections.sort(frequencies);
+        Collections.reverse(frequencies);
+
+        SListArray[] commonWords = new SListArray[n];
+        int freq = 0;
+        for (int i=0; i<n; i++) {
+            freq = frequencies.get(i);
+            // loop through the dictionary to look for the key that matches this value
+            for (String s: dict.keySet()) {
+                if (dict.get(s) == freq) {
+                    commonWords[i].add(commonWords[i].size()-1, s);
+                }
+            }
+        }
+        return commonWords;
     }
 }
